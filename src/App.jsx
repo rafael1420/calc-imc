43372cb1893d8f98d1cd107dd369fa10a5b163e1
unknown <rafuzelz2007@gmail.com>
2025-04-14 15:20:59 -0300

    Ultimo dia - Resultado final(Projeto Concluído)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { data } from './data/data';
-import { use, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import ImcCalc from './components/ImcCalc';
-import imcTable from './components/imcTable';
+import ImcTable from './components/imcTable'; // corrigido
 
 function App() {
   const calcImc = (e, height, weight) => {
@@ -13,14 +13,27 @@ function App() {
     const weightFloat = +weight.replace(",", ".");
     const heightFloat = +height.replace(",", ".");
 
-    const imcResult = (weightFloat /(heightFloat * heightFloat)).toFixed(1);
-
-
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
 
     setImc(imcResult);
 
+    data.forEach((item) => {
+      if(imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setinfoClass(item.infoClass);
+      }
+    });
+
+    if (!info) return;
+
   };
 
+  const resetCalc = (e) => {
+    e.preventDefault()
+    setImc("");
+    setInfo("");
+    setinfoClass("");
+  }
 
   const [imc, setImc] = useState("");
   const [info, setInfo] = useState('');
@@ -30,13 +43,19 @@ function App() {
     <>
       <div className='container'>
         {!imc ? (
-          <ImcCalc calcImc={calcImc} />
+          <ImcCalc calcImc={calcImc}/>
         ) : (
-          <imcTable/>
+          <ImcTable 
+          data={data} 
+          imc={imc} 
+          info={info} 
+          infoClass={infoClass}
+          resetCalc={resetCalc}
+          /> // Estava errado
         )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
